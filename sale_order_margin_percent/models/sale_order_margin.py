f0ca1a3b433850ded45486690989c72b5ffaa449
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-06-02 05:49:25 -0400

    WIP fix margin percent and move fields

--- sale_order_margin_percent/models/sale_order_margin.py
+++ sale_order_margin_percent/models/sale_order_margin.py
@@ -10,10 +10,27 @@ class SaleOrder(models.Model):
         string='Percent',
         compute='_compute_percent',
         digits=(16, 2),
-        )
+    )
 
     @api.depends('margin', 'amount_untaxed')
     def _compute_percent(self):
         for order in self:
-            if order.margin and order.amount_untaxed:
+            if order.margin and order.amount_untaxed > 0:
                 order.percent = (order.margin / order.amount_untaxed) * 100
+            else:
+                order.percent = 0
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    margin_percent = fields.Float(string="Margin Percent",
+                                  compute='_compute_margin_percent')
+
+    @api.depends('margin', 'price_subtotal')
+    def _compute_margin_percent(self):
+        for line in self:
+            if line.price_subtotal and line.margin > 0:
+                line.margin_percent = (line.margin / line.price_subtotal) * 100.
+            else:
+                line.margin_percent = 0
