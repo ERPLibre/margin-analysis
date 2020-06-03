0671d61ae63534ca3c0b35089a43365f1d3b98e9
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-06-02 23:41:29 -0400

    Rename margin_edit to margin_monetary, and add margin_switch_method

--- sale_margin_editor/models/sale_order_margin.py
+++ sale_margin_editor/models/sale_order_margin.py
@@ -1,6 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, api, fields
+from odoo import _, models, api, fields
 from odoo.addons import decimal_precision as dp
 
 
@@ -19,9 +19,13 @@ class SaleOrder(models.Model):
         string="Service Margin (%)",
         digits=dp.get_precision('Product Price'),
         help="Update service margin only. Set to 0 to resume global updating effect.")
+    margin_switch_method = fields.Selection(selection=[
+        ('0', _('Percent')),
+        ('1', _('Monetary')),
+    ], string='Switch Method Margin', default='0', required=True)
 
     @api.onchange('margin_global_edit')
-    def margin_edit_change(self):
+    def margin_monetary_change(self):
         if not self.margin_global_product:
             self.margin_global_product = self.margin_global_edit
             self.margin_product_edit_change(ignore_update_sale_margin=True)
@@ -35,7 +39,7 @@ class SaleOrder(models.Model):
         has_change = False
         for line in self.order_line:
             if line.product_id.type != "service" and not line.margin_lock:
-                self._update_line_margin_edit(line, self.margin_global_product)
+                self._update_line_margin_monetary(line, self.margin_global_product)
                 has_change = True
         if not ignore_update_sale_margin and has_change:
             self._product_margin()
@@ -45,32 +49,52 @@ class SaleOrder(models.Model):
         has_change = False
         for line in self.order_line:
             if line.product_id.type == "service" and not line.margin_lock:
-                self._update_line_margin_edit(line, self.margin_global_service)
+                self._update_line_margin_monetary(line, self.margin_global_service)
                 has_change = True
             if not ignore_update_sale_margin and has_change:
                 self._product_margin()
 
-    def _update_line_margin_edit(self, line, marge):
+    def _update_line_margin_monetary(self, line, marge):
         marge_percent = marge / 100
         if line.purchase_price:
-            line.margin_edit = marge_percent * line.purchase_price
+            line.margin_monetary = marge_percent * line.purchase_price
         else:
-            line.margin_edit = marge_percent
-        line.price_unit = line.purchase_price + line.margin_edit
+            line.margin_monetary = marge_percent
+        line.price_unit = line.purchase_price + line.margin_monetary
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    margin_edit = fields.Float(string="Margin ($)",
-                               digits=dp.get_precision('Product Price'))
+    margin_monetary = fields.Float(string="Margin ($)",
+                                   digits=dp.get_precision('Product Price'))
 
     margin_lock = fields.Boolean(string="Lock margin", default=False,
                                  help="Don't be affect by global marge.")
 
-    @api.onchange('margin_edit', 'purchase_price')
-    def margin_edit_change(self):
-        self.price_unit = self.purchase_price + self.margin_edit
+    @api.onchange('purchase_price')
+    def margin_purchase_price_change(self):
+        if self.order_id.margin_switch_method == '0':
+            # Percent
+            if self.margin_lock:
+                self.price_unit = self.purchase_price * (1 + self.margin_monetary / (
+                        self.price_unit - self.margin_monetary))
+            else:
+                if self.product_id.type == "service":
+                    self.price_unit = self.purchase_price * (
+                        1 + self.order_id.margin_global_service)
+                else:
+                    self.price_unit = self.purchase_price * (
+                        1 + self.order_id.margin_global_product / 100)
+        elif self.order_id.margin_switch_method == '1':
+            # Monetary
+            self.price_unit = self.purchase_price + self.margin_monetary
+        else:
+            raise ValueError(_("Unknown value of margin_switch_method"))
+
+    @api.onchange('margin_monetary')
+    def margin_monetary_change(self):
+        self.price_unit = self.purchase_price + self.margin_monetary
         self._product_margin()
 
     # @api.onchange('product_uom_qty')
@@ -81,4 +105,4 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('price_unit')
     def price_unit_margin_change(self):
-        self.margin_edit = self.price_unit - self.purchase_price
+        self.margin_monetary = self.price_unit - self.purchase_price
